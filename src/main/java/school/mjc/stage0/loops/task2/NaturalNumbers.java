a8591fd0209e2e7736d99d29ccfc344ee2a793c2
Romchik98 <38857653+Romchik98@users.noreply.github.com>
2022-09-07 10:57:08 +0300

    NaturalNumbers.java fix

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -3,6 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
         int counter = 1;
+        System.out.println("0");
         while (counter <= lastPrinted) {
             System.out.println(counter);
             counter++;
